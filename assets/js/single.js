080085bed21716a4e17d9a7a502f21996986e3e5
Bert <robertgill82@gmail.com>
2020-10-26 10:00:34 -0700

    getrepoissues and getreponame updated in single.js

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,17 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
-
-var displayWarning = function (repo) {
-  // add text to warning container
-  limitWarningEl.textContent = "To see more than 30 issues, visit ";
-  var linkEl = document.createElement("a");
-  linkEl.textContent = "See More Issues on GitHub.com";
-  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
-  linkEl.setAttribute("target", "_blank");
-
-  // append to warning container
-  limitWarningEl.appendChild(linkEl);
-};
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function (repo) {
   console.log(repo);
@@ -27,18 +16,26 @@ var getRepoIssues = function (repo) {
 
         // check if api has paginated issues
         if (response.headers.get("Link")) {
-          console.log("displayWarning(repo)");
+          displayWarning(repo);
         }
       });
     } else {
-      alert("There was a problem with your request!");
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
 
-getRepoIssues("facebook/react");
-getRepoIssues("expressjs/express");
-getRepoIssues("angular/angular");
+var getRepoName = function () {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    document.location.replace("./index.html");
+  }
+};
 
 var displayIssues = function (issues) {
   if (issues.length === 0) {
@@ -73,4 +70,16 @@ var displayIssues = function (issues) {
 
     issueContainerEl.appendChild(issueEl);
   }
+};
+
+var displayWarning = function (repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
 };
